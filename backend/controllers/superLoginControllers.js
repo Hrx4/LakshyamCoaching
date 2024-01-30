70a332d8b7e3e376400a7ec34c43f9b5163bccf3
Kamal <dragkamal71@gmail.com>
2024-01-30 23:09:10 +0530

    Small changes added

--- backend/controllers/superLoginControllers.js
+++ backend/controllers/superLoginControllers.js
@@ -5,8 +5,10 @@ const jwt = require('jsonwebtoken')
 const superLogin = asyncHandler(async(req , res)=>{
     const {userName , password} = req.body;
     if(userName!== "usename" && password!=="password") return res.status(400).json({message:"Invalid User"})
-    const token = jwt.sign({useName : userName , password:password} , process.env.JWT_SECRET)
-    res.status(201).json({useName : userName , token:token})
+    // const token = jwt.sign({useName : userName , password:password} , process.env.JWT_SECRET)
+    res.status(201).json({useName : userName 
+        // , token:token
+    })
 })
 
 
